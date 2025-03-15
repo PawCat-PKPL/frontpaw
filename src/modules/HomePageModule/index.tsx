18fa0562996d59ff6c7ce7c8ce99cff0879cb818
narawaa <nashwa.ghania@ui.ac.id>
2025-03-15 12:44:25 +0700

    fix: homepage layout

--- src/modules/HomePageModule/index.tsx
+++ src/modules/HomePageModule/index.tsx
@@ -2,12 +2,12 @@ import ConverterForm from "./sections/ConverterForm";
 
 export const HomePageModule = () => {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+    <div className="flex flex-col items-center justify-center min-h-screen px-5 py-20 lg:px-28">
+      <main className="py-10">
         HALO
         {/* Currency Converter */}
         <section>
-          <h2 className="converter-title">Currency Converter</h2>
+          <h2>Currency Converter</h2>
           <ConverterForm />
         </section>
       </main>
