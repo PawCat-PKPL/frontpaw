932d5194e122de4c17f61ca28398dda9c647f5c1
narawaa <nashwa.ghania@ui.ac.id>
2025-03-31 22:53:07 +0700

    fix: text color

--- src/components/layout/footer/index.tsx
+++ src/components/layout/footer/index.tsx
@@ -4,14 +4,14 @@ export const Footer = () => {
   return (
     <footer className="rounded-lg shadow-sm m-4">
       <div className="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
-        <span className="text-sm sm:text-center">
+        <span className="text-sm sm:text-center font-semibold text-[#742f02]">
           © 2025{" "}
           <a href="#" className="hover:underline">
             PawCat
           </a>
           . All Rights Reserved.
         </span>
-        <ul className="flex flex-wrap items-center mt-3 text-sm font-medium sm:mt-0">
+        <ul className="flex flex-wrap items-center mt-3 text-sm font-bold text-[#742f02] sm:mt-0">
           <li>
             <a href="#" className="hover:underline me-4 md:me-6">
               About
