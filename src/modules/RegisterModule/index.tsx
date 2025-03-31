891924a7f7886a63a73fe4d4d47790a154910452
narawaa <nashwa.ghania@ui.ac.id>
2025-04-01 00:35:52 +0700

    feat: new register

--- src/modules/RegisterModule/index.tsx
+++ src/modules/RegisterModule/index.tsx
@@ -2,9 +2,9 @@
 import { useState, FormEvent } from "react";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
-
 import { useAuth } from "@/hooks/useAuth";
 import { toast, Toaster } from "@/components/sonner";
+import Image from "next/image";
 
 export const RegisterPageModule = () => {
   const { registerUser } = useAuth();
@@ -49,124 +49,144 @@ export const RegisterPageModule = () => {
   };
 
   return (
-    <section className="flex flex-col px-5 py-20 lg:px-28 bg-gray-900 min-h-screen items-center justify-center">
-      <div className="w-full max-w-md rounded-lg shadow bg-gray-800 p-8">
-        <h1 className="text-2xl font-bold text-white mb-6">
-          Create an account
-        </h1>
+    <section className="flex flex-col px-5 py-20 lg:px-28 max-lg:bg-[#FAA307]/20 min-h-screen items-center justify-center">
+      <Image
+        src="/Divide2.png"
+        className="max-lg:hidden absolute top-16 right-0 w-[80%] h-full object-cover -z-10"
+        alt="Divide2"
+        width={600}
+        height={300}
+      />
+      <div className="flex items-center justify-center lg:grid lg:grid-cols-2 gap-8 w-full max-w-6xl">
+        <Image
+          src="/Register.png"
+          className="max-lg:hidden"
+          alt="Register"
+          width={900}
+          height={600}
+        />
 
-        <Toaster />
-
-        <form className="space-y-6" onSubmit={handleSubmit}>
-          <Toaster />
-          <div>
-            <label
-              htmlFor="email"
-              className="block mb-2 text-sm font-medium text-white"
-            >
-              Email
-            </label>
-            <input
-              type="email"
-              id="email"
-              value={formData.email}
-              onChange={handleChange}
-              placeholder="name@company.com"
-              className="w-full p-2.5 border text-sm rounded-lg bg-gray-700 border-gray-600 placeholder-gray-400 text-white focus:ring-blue-500 focus:border-blue-500"
-              required
-            />
-          </div>
-
-          <div>
-            <label
-              htmlFor="username"
-              className="block mb-2 text-sm font-medium text-white"
-            >
-              Username
-            </label>
-            <input
-              type="text"
-              id="username"
-              placeholder="JohnDoe"
-              value={formData.username}
-              onChange={handleChange}
-              className="w-full p-2.5 border text-sm rounded-lg bg-gray-700 border-gray-600 placeholder-gray-400 text-white focus:ring-blue-500 focus:border-blue-500"
-              required
-            />
-          </div>
-
-          <div>
-            <label
-              htmlFor="full_name"
-              className="block mb-2 text-sm font-medium text-white"
-            >
-              Full Name
-            </label>
-            <input
-              type="text"
-              id="full_name"
-              placeholder="John Doe"
-              value={formData.full_name}
-              onChange={handleChange}
-              className="w-full p-2.5 border text-sm rounded-lg bg-gray-700 border-gray-600 placeholder-gray-400 text-white focus:ring-blue-500 focus:border-blue-500"
-              required
-            />
-          </div>
-
-          <div>
-            <label
-              htmlFor="password"
-              className="block mb-2 text-sm font-medium text-white"
-            >
-              Password
-            </label>
-            <input
-              type="password"
-              id="password"
-              placeholder="••••••••"
-              value={formData.password}
-              onChange={handleChange}
-              className="w-full p-2.5 border text-sm rounded-lg bg-gray-700 border-gray-600 placeholder-gray-400 text-white focus:ring-blue-500 focus:border-blue-500"
-              required
-            />
-          </div>
-
-          <div>
-            <label
-              htmlFor="password2"
-              className="block mb-2 text-sm font-medium text-white"
-            >
-              Confirm Password
-            </label>
-            <input
-              type="password"
-              id="password2"
-              placeholder="••••••••"
-              value={formData.password2}
-              onChange={handleChange}
-              className="w-full p-2.5 text-sm rounded-lg bg-gray-700 border-gray-600 placeholder-gray-400 text-white focus:ring-blue-500 focus:border-blue-500"
-              required
-            />
-          </div>
-
-          <button
-            type="submit"
-            disabled={isLoading}
-            className="w-full text-white focus:ring-4 focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center bg-blue-600 hover:bg-blue-700 focus:ring-blue-800"
-          >
-            Create an account
-          </button>
-
-          <p className="text-sm font-light text-gray-400">
-            Already have an account?{" "}
-            <Link
-              href="/login"
-              className="font-medium hover:underline text-blue-500"
-            >
-              Login here
-            </Link>
+        <div className="w-full max-w-md rounded-xl shadow bg-white p-8">
+          <h1 className="text-2xl font-bold text-center mb-1">
+            Create Your Account
+          </h1>
+          <p className="text-sm text-gray-500 mb-6 text-center">
+            Please fill in the details to register
           </p>
-        </form>
+
+          <Toaster />
+
+          <form className="space-y-6" onSubmit={handleSubmit}>
+            <Toaster />
+            <div>
+              <label
+                htmlFor="email"
+                className="block mb-2 text-sm font-medium"
+              >
+                Email
+              </label>
+              <input
+                type="email"
+                id="email"
+                value={formData.email}
+                onChange={handleChange}
+                placeholder="name@company.com"
+                className="w-full p-2.5 border text-sm rounded-lg bg-gray-100 border-gray-300 placeholder-gray-400 focus:ring-blue-500 focus:border-blue-500"
+                required
+              />
+            </div>
+
+            <div>
+              <label
+                htmlFor="username"
+                className="block mb-2 text-sm font-medium"
+              >
+                Username
+              </label>
+              <input
+                type="text"
+                id="username"
+                placeholder="JohnDoe"
+                value={formData.username}
+                onChange={handleChange}
+                className="w-full p-2.5 border text-sm rounded-lg bg-gray-100 border-gray-300 placeholder-gray-400 focus:ring-blue-500 focus:border-blue-500"
+                required
+              />
+            </div>
+
+            <div>
+              <label
+                htmlFor="full_name"
+                className="block mb-2 text-sm font-medium"
+              >
+                Full Name
+              </label>
+              <input
+                type="text"
+                id="full_name"
+                placeholder="John Doe"
+                value={formData.full_name}
+                onChange={handleChange}
+                className="w-full p-2.5 border text-sm rounded-lg bg-gray-100 border-gray-300 placeholder-gray-400 focus:ring-blue-500 focus:border-blue-500"
+                required
+              />
+            </div>
+
+            <div>
+              <label
+                htmlFor="password"
+                className="block mb-2 text-sm font-medium"
+              >
+                Password
+              </label>
+              <input
+                type="password"
+                id="password"
+                placeholder="••••••••"
+                value={formData.password}
+                onChange={handleChange}
+                className="w-full p-2.5 border text-sm rounded-lg bg-gray-100 border-gray-300 placeholder-gray-400 focus:ring-blue-500 focus:border-blue-500"
+                required
+              />
+            </div>
+
+            <div>
+              <label
+                htmlFor="password2"
+                className="block mb-2 text-sm font-medium"
+              >
+                Confirm Password
+              </label>
+              <input
+                type="password"
+                id="password2"
+                placeholder="••••••••"
+                value={formData.password2}
+                onChange={handleChange}
+                className="w-full p-2.5 text-sm rounded-lg bg-gray-100 border border-gray-300 placeholder-gray-400 focus:ring-blue-500 focus:border-blue-500"
+                required
+              />
+            </div>
+
+            <button
+              type="submit"
+              disabled={isLoading}
+              className="w-full text-white focus:ring-4 focus:outline-none font-bold rounded-lg text-sm px-5 py-2.5 text-center bg-[#FF7A00] hover:bg-[#F48C06] focus:ring-blue-800"
+            >
+              Register Now
+            </button>
+
+            <p className="text-sm text-gray-500 text-center">
+              Already have an account?{" "}
+              <Link
+                href="/login"
+                className="font-medium hover:underline text-blue-500"
+              >
+                Login here
+              </Link>
+            </p>
+          </form>
+        </div>
       </div>
     </section>
   );
