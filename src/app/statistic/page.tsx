c597f6cc346d1cfa5eb90164bdfe6fd390b8c362
almerazka <almer.azka12@gmail.com>
2025-04-14 18:38:06 +0700

    feat: add StatisticPage component with module integration

---
+++ src/app/statistic/page.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+import StatisticPageModule from "@/modules/StatisticModule";
+
+const StatisticPage = () => {
+  return <StatisticPageModule />;
+};
+
+export default StatisticPage;
